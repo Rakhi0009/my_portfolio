f5620f9155279e1795a33ea71b64711fb50d3ae0
[Rakhi] <[rakhinegi1997@gmail.com]>
2019-11-11 19:45:14 +0530

    Added new projects and blogs

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
@@ -13,7 +13,7 @@ class Blog(models.Model):
         return self.title
 
     def summary(self):
-        return self.body[:100]
+        return self.body[:300]
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
